260d0a2e3af14a280ded629a2e8633ff5e432ee8
EmmySavage <emmanuelugochukwu567@gmail.com>
2026-08-31 15:34:42 +0100

    Day 7:Python Variables and data representation

---
+++ day7/day7.md
@@ -0,0 +1,5 @@
+List vs Dictionary
+
+A list stores multiple values in an ordered sequence. I would use a list when I mainly need a collection of items, such as a list of book titles.
+
+A dictionary stores data as a value in pairs. I would use a dictionary when I need to represent a complete record with different pieces of information, such as a book’s ID, title, author, and availability of a particular novel.
---
+++ day7/variables.py
@@ -0,0 +1,21 @@
+book_id = 1
+title ="Things Fall Apart"
+price = 2500.50
+available = True
+print("book_id:",book_id,type (book_id))
+print("title:", title, type (title))
+print("price",price, type (price))
+print ("available", available, type (available))
+book_titles =[
+    "Things Fall Apart",
+    "The Hobbit",
+    "Animal Farm"
+]
+book = {
+"id": 1,
+"title": "Things Fall Apart",
+"author":"Chinua Achebe",
+"available": True
+}
+print("Book titles:",book_titles)
+print ("Full book record:",book)
